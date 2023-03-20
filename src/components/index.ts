43ef76af7f348f8ca12ca11ef831d3d9afe6cd61
yoosion030 <rsy5487@naver.com>
2023-03-20 12:22:00 +0900

    [#2] feature: 페이지네이션 구현
    페이지네이션 컴포넌트를 만들어 state를 넘겨주는 형식으로 구현

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,3 @@
 export { default as Order } from './Order';
+export { default as Pagination } from './Pagination';
+export { default as MainLayout } from './common/MainLayout';
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,15 +1,24 @@
-import { Order } from 'components';
+import { MainLayout, Order, Pagination } from 'components';
 import { useLoaderData } from 'react-router-dom';
 import { OrderDataType } from 'types/order';
-import { Table, Thead, Tbody, Tr, Th, Td, TableContainer } from '@chakra-ui/react';
+import { Table, Thead, Tbody, Tr, Th, TableContainer } from '@chakra-ui/react';
 import { useEffect, useState } from 'react';
+
+const MAX_SIZE = 50;
 
 const MainPage = () => {
   const orderData = useLoaderData() as OrderDataType[];
-  const [test, setTest] = useState<OrderDataType[]>(orderData.slice(0, 50));
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const startPage = MAX_SIZE * currentPage - MAX_SIZE;
+  const finishPage = MAX_SIZE * currentPage;
+
+  console.log(startPage, finishPage, currentPage);
+
+  const [test, setTest] = useState<OrderDataType[]>(orderData.slice(startPage, finishPage));
   const [isAscending, setIsAscending] = useState<boolean>(true);
   const [isDateAscending, setIsDateAscending] = useState<boolean | null>(null);
-  console.log(`id: ${isAscending} time: ${isDateAscending}`);
+
+  useEffect(() => setTest(orderData.slice(startPage, finishPage)), [startPage, finishPage]);
 
   useEffect(() => {
     if (!isAscending) {
@@ -32,30 +41,39 @@ const MainPage = () => {
       setTest(sortedData);
     }
   }, [isDateAscending]);
+
   return (
-    <TableContainer width='1200px'>
-      <Table variant='simple'>
-        <Thead>
-          <Tr>
-            <Th onClick={() => setIsAscending(!isAscending)} cursor={'pointer'}>
-              주문번호 {isAscending ? '(오름차순)' : '(내림차순)'}
-            </Th>
-            <Th onClick={() => setIsDateAscending(!isDateAscending)} cursor={'pointer'}>
-              거래시간 {isDateAscending !== null && (isDateAscending ? '(오름차순)' : '(내림차순)')}
-            </Th>
-            <Th>주문처리상태</Th>
-            <Th>고객번호</Th>
-            <Th>고객이름</Th>
-            <Th>가격</Th>
-          </Tr>
-        </Thead>
-        <Tbody>
-          {test.map(v => (
-            <Order {...v} key={v.id} />
-          ))}
-        </Tbody>
-      </Table>
-    </TableContainer>
+    <MainLayout>
+      <TableContainer width='1200px'>
+        <Table variant='simple'>
+          <Thead>
+            <Tr>
+              <Th onClick={() => setIsAscending(!isAscending)} cursor={'pointer'}>
+                주문번호 {isAscending ? '(오름차순)' : '(내림차순)'}
+              </Th>
+              <Th onClick={() => setIsDateAscending(!isDateAscending)} cursor={'pointer'}>
+                거래시간
+                {isDateAscending !== null && (isDateAscending ? '(오름차순)' : '(내림차순)')}
+              </Th>
+              <Th textAlign='center'> 주문처리상태</Th>
+              <Th textAlign='center'>고객번호</Th>
+              <Th>고객이름</Th>
+              <Th>가격</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {test.map(v => (
+              <Order {...v} key={v.id} />
+            ))}
+          </Tbody>
+        </Table>
+      </TableContainer>
+      <Pagination
+        currentPage={currentPage}
+        totalPages={orderData.length / MAX_SIZE}
+        onPageChange={setCurrentPage}
+      />
+    </MainLayout>
   );
 };
 
