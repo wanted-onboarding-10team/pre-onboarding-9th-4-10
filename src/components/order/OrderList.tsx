c8264809241d5a1ffbe48773a15659d58a36bcb2
Shinpanda <duldulgo1@gmail.com>
2023-03-21 01:08:20 +0900

    feature :  필터링 기능 추가 주문 처리 상태를 버튼으로 선택할 수 있도록 구현 [#3]

--- src/components/order/OrderList.tsx
+++ src/components/order/OrderList.tsx
@@ -24,7 +24,16 @@ const pagingOffset = 10;
 const OrderList = ({ orderLists }: OrderListParam) => {
   const [searchParams, setSearchParams] = useSearchParams();
   const sortKey: string[] = searchParams.getAll('sort');
-  const totalOrders = orderLists ? orderLists.length - 1 : 0;
+  const filter = searchParams.get('filter');
+  const viewDatas =
+    filter !== null ? orderLists.filter(order => '' + order.status === filter) : orderLists;
+
+  sortKey.forEach(sortKey => {
+    viewDatas.sort((a, b) =>
+      (sortKey === 'id' ? a.id > b.id : a.transaction_time > b.transaction_time) ? -1 : 1,
+    );
+  });
+  const totalOrders = viewDatas ? viewDatas.length - 1 : 0;
   const maxIndex = Math.floor(totalOrders / pageSize) - 1;
   const pagingIndex = parseInt(searchParams.get('page') ?? '1') - 1;
   const startPageIndex = pagingIndex - ((pagingIndex - 1) % 10) - 1;
@@ -59,7 +68,7 @@ const OrderList = ({ orderLists }: OrderListParam) => {
     });
   };
 
-  const onClickSortKey = (key: string) => {
+  const setSortKey = (key: string) => {
     const newSortKey = sortKey.includes(key)
       ? sortKey.filter(value => value !== key)
       : [...sortKey, key];
@@ -70,31 +79,50 @@ const OrderList = ({ orderLists }: OrderListParam) => {
     setSearchParams(searchParams);
   };
 
-  sortKey.forEach(sortKey => {
-    orderLists.sort((a, b) =>
-      (sortKey === 'id' ? a.id > b.id : a.transaction_time > b.transaction_time) ? -1 : 1,
-    );
-  });
-
+  const setFiltering = (status: boolean) => {
+    setSearchParams(searchParams => {
+      if (filter === '' + status) {
+        searchParams.delete('filter');
+      } else {
+        searchParams.set('filter', '' + status);
+      }
+      return searchParams;
+    });
+  };
   return (
     <>
       <Box pb={5} pr={5} display={'flex'} gap={2} alignItems='center'>
-        <Text>정렬</Text>
+        <Text width='24'>정렬</Text>
         <Button
-          onClick={() => onClickSortKey('id')}
+          onClick={() => setSortKey('id')}
           backgroundColor={sortKey.includes('id') ? 'gray.300' : 'gray.100'}
         >
           주문번호
         </Button>
         <Button
-          onClick={() => onClickSortKey('transaction_time')}
+          onClick={() => setSortKey('transaction_time')}
           backgroundColor={sortKey.includes('transaction_time') ? 'gray.300' : 'gray.100'}
         >
           거래일 & 거래시간
         </Button>
       </Box>
+      <Box pb={5} pr={5} display={'flex'} gap={2} alignItems='center'>
+        <Text width='24'>주문 처리 상태</Text>
+        <Button
+          onClick={() => setFiltering(true)}
+          backgroundColor={filter === 'true' ? 'gray.300' : 'gray.100'}
+        >
+          처리
+        </Button>
+        <Button
+          onClick={() => setFiltering(false)}
+          backgroundColor={filter === 'false' ? 'gray.300' : 'gray.100'}
+        >
+          비처리
+        </Button>
+      </Box>
       <Box pb={5} pr={5}>
-        <Text textAlign={'right'}>주문 건 : {orderLists.length} 건</Text>
+        <Text textAlign={'right'}>주문 건 : {viewDatas.length} 건</Text>
       </Box>
       <Table variant='striped'>
         <Thead>
@@ -108,7 +136,7 @@ const OrderList = ({ orderLists }: OrderListParam) => {
           </Tr>
         </Thead>
         <Tbody>
-          {orderLists.slice(pageSize * pagingIndex, pageSize * pagingIndex + pageSize).map(item => (
+          {viewDatas.slice(pageSize * pagingIndex, pageSize * pagingIndex + pageSize).map(item => (
             <Tr key={item.id}>
               <Td>{item.id}</Td>
               <Td>{item.transaction_time}</Td>
