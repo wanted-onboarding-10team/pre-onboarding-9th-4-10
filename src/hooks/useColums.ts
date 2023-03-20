75cfa101369d1b2edf0e4288dd8c8654a6b056a5
DESKTOP-1V9OQ1L\GIGABYTE <emma05127@gmail.com>
2023-03-20 17:29:06 +0900

    [#2] feature: Table Colums hook 추가
    React Table 에서 필요한 Colums를 생성하는 hook을 추가
    관련 로직의 양이 많아서 hook을 이용해 분리

---
+++ src/hooks/useColums.ts
@@ -0,0 +1,45 @@
+import { useMemo } from 'react';
+import { createColumnHelper } from '@tanstack/react-table';
+import { DataResponse } from 'types';
+
+function useColums() {
+  const columnHelper = createColumnHelper<DataResponse>();
+
+  const columns = useMemo(
+    () => [
+      columnHelper.accessor('id', {
+        header: '주문 번호',
+        cell: info => info.getValue(),
+      }),
+      columnHelper.accessor('customer_id', {
+        header: '고객 번호',
+        cell: info => info.getValue(),
+      }),
+      columnHelper.accessor('customer_name', {
+        header: '고객 이름',
+        cell: info => info.getValue(),
+      }),
+      columnHelper.accessor('currency', {
+        header: '가격',
+        cell: info => info.getValue(),
+      }),
+      columnHelper.accessor('status', {
+        header: '주문 처리 상태',
+        cell: info => info.getValue().toString(),
+      }),
+      columnHelper.accessor('transaction_time', {
+        header: '거래 날짜',
+        cell: info => info.getValue().split(' ')[0],
+      }),
+      columnHelper.accessor('transaction_time', {
+        header: '거래 시간',
+        cell: info => info.getValue().split(' ')[1],
+      }),
+    ],
+    [],
+  );
+
+  return columns;
+}
+
+export default useColums;
