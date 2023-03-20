0c8df5d3edc5c329563cbdc6313244e51fa9cd67
yoosion030 <rsy5487@naver.com>
2023-03-20 16:51:15 +0900

    [#2] refactor: 변수명 변경
    임의로 지었던 test를 orderData로 변경

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,19 +1,22 @@
 import { MainLayout, Order, Pagination, SearchInput } from 'components';
 import { useLoaderData } from 'react-router-dom';
 import { OrderDataType } from 'types/order';
-import { Table, Thead, Tbody, Tr, Th, TableContainer } from '@chakra-ui/react';
+import { Table, Thead, Tbody, Tr, Th, TableContainer, Flex } from '@chakra-ui/react';
 import { useEffect, useState } from 'react';
 
 // 페이지 최대 한도
 const MAX_SIZE = 50;
+const TODAY = new Date('2023.03.08').toDateString();
 
 const MainPage = () => {
-  const orderData = useLoaderData() as OrderDataType[];
+  const orderDataResponse = useLoaderData() as OrderDataType[];
   const [currentPage, setCurrentPage] = useState<number>(1);
   const startPage = MAX_SIZE * currentPage - MAX_SIZE;
   const finishPage = MAX_SIZE * currentPage;
 
-  const [test, setTest] = useState<OrderDataType[]>(orderData.slice(startPage, finishPage));
+  const [orderData, setOrderData] = useState<OrderDataType[]>(
+    orderDataResponse.filter(v => new Date(v.transaction_time).toDateString() === TODAY),
+  );
 
   const [isAscending, setIsAscending] = useState<boolean>(true);
   const [isDateAscending, setIsDateAscending] = useState<boolean | null>(null);
@@ -23,16 +26,16 @@ const MainPage = () => {
   // 아이디 정렬
   useEffect(() => {
     if (!isAscending) {
-      setTest(test.sort((a, b) => a.id - b.id));
+      setOrderData(orderData.sort((a, b) => a.id - b.id));
     } else {
-      setTest(test.sort((a, b) => b.id - a.id));
+      setOrderData(orderData.sort((a, b) => b.id - a.id));
     }
   }, [isAscending]);
 
   // 날짜 정렬
   useEffect(() => {
     if (isDateAscending !== null) {
-      const sortedData = test.sort((a, b) => {
+      const sortedData = orderData.sort((a, b) => {
         if (!isDateAscending) {
           return new Date(a.transaction_time).getTime() - new Date(b.transaction_time).getTime();
         } else {
@@ -40,7 +43,7 @@ const MainPage = () => {
         }
       });
 
-      setTest(sortedData);
+      setOrderData(sortedData);
     }
   }, [isDateAscending]);
 
@@ -48,30 +51,40 @@ const MainPage = () => {
   useEffect(() => {
     if (searchWord === '') {
       setCurrentPage(1);
-      setTest(orderData);
+      setOrderData(orderDataResponse);
       return;
     }
 
-    const filteredData = orderData.filter(v =>
+    const filteredData = orderDataResponse.filter(v =>
       v.customer_name.toLowerCase().includes(searchWord.toLowerCase()),
     );
-    setTest(filteredData);
+    setOrderData(filteredData);
   }, [searchWord]);
 
   return (
     <MainLayout>
       <SearchInput onSearchWordChange={setSearchWord} />
+
+      <Flex>
+        <p>최근순</p>
+        <p>오래된순</p>
+
+        <p>번호 오름차순</p>
+        <p>번호 내림차순</p>
+
+        <p>주문처리상태</p>
+      </Flex>
+      <Flex>
+        <p>오늘</p>
+        <p>{TODAY}</p>
+      </Flex>
+
       <TableContainer width='1200px'>
         <Table variant='simple'>
           <Thead>
             <Tr>
-              <Th onClick={() => setIsAscending(!isAscending)} cursor={'pointer'}>
-                주문번호 {isAscending ? '(오름차순)' : '(내림차순)'}
-              </Th>
-              <Th onClick={() => setIsDateAscending(!isDateAscending)} cursor={'pointer'}>
-                거래시간
-                {isDateAscending !== null && (isDateAscending ? '(오름차순)' : '(내림차순)')}
-              </Th>
+              <Th>주문번호</Th>
+              <Th>거래시간</Th>
               <Th textAlign='center'> 주문처리상태</Th>
               <Th textAlign='center'>고객번호</Th>
               <Th>고객이름</Th>
@@ -79,17 +92,16 @@ const MainPage = () => {
             </Tr>
           </Thead>
           <Tbody>
-            {test.slice(startPage, finishPage).map(v => (
+            {orderData.slice(startPage, finishPage).map(v => (
               <Order {...v} key={v.id} />
             ))}
           </Tbody>
         </Table>
       </TableContainer>
+
       <Pagination
         currentPage={currentPage}
-        totalPages={
-          searchWord ? parseInt((test.length / MAX_SIZE).toString()) : orderData.length / MAX_SIZE
-        }
+        totalPages={parseInt((orderData.length / MAX_SIZE).toString())}
         onPageChange={setCurrentPage}
       />
     </MainLayout>
