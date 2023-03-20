2e06592473451c57d54a38ac5ddaa7f23e7a3a2e
Ryong-E <think8867@gmail.com>
2023-03-20 14:45:59 +0900

    [#2] style: global style 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { RouterProvider } from 'react-router-dom';
 import router from 'router';
+import GlobalStyled from 'styles/GlobalStyle';
 
 function App() {
   return (
     <>
+      <GlobalStyled />
       <RouterProvider router={router} />
     </>
   );
