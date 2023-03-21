dbab4fe8d9a48e680be259bd89c663603494413b
hongsoom <hongsoom@kakao.com>
2023-03-21 15:29:14 +0900

    [#2 #3] feature : 페이지네이션, 정렬, 한 페이지당 50개 출력할 수 있게 하는 함수 로직 usePageData hook
    Math.ceil을 이용하여 배열의 길이 / 50 => 페이지 수
    slice을 이용하여 page수의 따라 데이터 인덱스 계산 => 50, 100, 150 ...
    sort을 이용하여 id, time 기준점 마다 내림차순, 오름차순 => 바뀐 파일들을
    기억하는것이 관건..
    세가지 다 쿼리스트링을 기반

---
+++ src/hooks/usePageData.tsx
@@ -0,0 +1,58 @@
+import { useCallback, useEffect } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import useOrderData from './useOrderData';
+import { SINGLE_PAGE_SIZE, QUERY_KEY } from 'constants/index';
+import { OrderData } from 'types';
+
+const usePageData = () => {
+  const { pageList, setPageList } = useOrderData();
+
+  const [searchParams] = useSearchParams();
+
+  const page = Number(searchParams.get(QUERY_KEY.page)) || 1;
+  const sort = searchParams.get(QUERY_KEY.sort) || '';
+
+  const handleSortById = useCallback((order: string) => {
+    setPageList(prev => {
+      const newData = [...prev];
+      newData.sort((current: OrderData, next: OrderData) =>
+        order === 'asc' ? current.id - next.id : next.id - current.id,
+      );
+      return newData;
+    });
+  }, []);
+
+  const handleSortByTime = useCallback((order: string) => {
+    setPageList(prev => {
+      const newData = [...prev];
+      newData.sort((current: OrderData, next: OrderData) =>
+        order === 'asc'
+          ? new Date(current.transaction_time).getTime() - new Date(next.transaction_time).getTime()
+          : new Date(next.transaction_time).getTime() -
+            new Date(current.transaction_time).getTime(),
+      );
+      return newData;
+    });
+  }, []);
+
+  useEffect(() => {
+    if (sort) {
+      const [standard, order] = sort.split(':');
+      if (standard === QUERY_KEY.id) handleSortById(order as string);
+      if (standard === QUERY_KEY.time) handleSortByTime(order as string);
+    }
+  }, [sort]);
+
+  const pages = Math.ceil(pageList.length / SINGLE_PAGE_SIZE);
+
+  const start = (page - 1) * SINGLE_PAGE_SIZE;
+  const end = page * SINGLE_PAGE_SIZE;
+  const paginatedData = pageList?.slice(start, end);
+
+  return {
+    pages,
+    paginatedData,
+  };
+};
+
+export default usePageData;
