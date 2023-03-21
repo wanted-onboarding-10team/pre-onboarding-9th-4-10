f23ba12080c5142c2ae223eb9b2ce17f2b5607de
hongsoom <hongsoom@kakao.com>
2023-03-21 15:43:25 +0900

    [#2 #3] refactor : import 구문 개선

---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+export { default as Table } from './Table/Table';
+export { default as Pagination } from './Pagination/Pagination';
+export { default as Search } from './Search/Search';
+export { default as TableHeader } from './Table/TableHeader';
+export { default as TableBody } from './Table/TableBody';
+export { default as Header } from './Common/Header';
+export { default as Loading } from './Common/Loading';
---
+++ src/hooks/index.ts
@@ -0,0 +1,3 @@
+export { default as useOrderData } from './useOrderData';
+export { default as usePageData } from './usePageData';
+export { default as useQuery } from './useQuery';
