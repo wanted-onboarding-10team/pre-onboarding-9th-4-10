74b50159fa7d1e3a72b120a4dab43e4dca9f73e0
Ryong-E <think8867@gmail.com>
2023-03-21 18:57:32 +0900

    [#3] feature: 쿼리파라미터를 가지고 필터링해주는 custom Hook 생성
    fetching해온 데이터를 useFilter 커스텀훅에 넣어주게 되면 쿼리파라미터가 바뀔때마다 필터링되어 값을 뱉어주는 커스텀 훅
    HomePage : 커스텀 훅 적용과 search기능 적용 pagenation을 위해 값 구한뒤 커스텀훅에서 뱉어준 데이터를 slice하는 식으로 구현

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,34 +1,41 @@
 import MainLayout from 'components/layout/MainLayout';
+import SearchTool from 'components/table/searchTool/SearchTool';
 import OrderTable from 'components/table/OrderTable';
 import Pagenation from 'components/table/Pagenation';
 import { PAGE } from 'constants/page';
+import useFilter from 'hooks/useFilter';
 import useQuery from 'hooks/useQuery';
-import { useEffect } from 'react';
 import { useSearchParams } from 'react-router-dom';
+import styled from 'styled-components';
 
 const HomePage = () => {
-  const { data } = useQuery('/data/mock_data.json');
-  const [query, setQuery] = useSearchParams();
-
+  const { data: responseData } = useQuery('/data/mock_data.json');
+  const [query] = useSearchParams();
+  const fabricatedData = useFilter(responseData);
   const pageNumber = Number(query.getAll('page')) || 1;
   const startPageNumber = (pageNumber - 1) * PAGE.maxview;
   const endPageNumber = pageNumber * PAGE.maxview;
-  const splitPage = data?.slice(startPageNumber, endPageNumber);
-
-  useEffect(() => {
-    if (!query.get('page')) {
-      setQuery({ page: '' });
-    }
-  }, []);
 
   return (
     <MainLayout>
-      <section>
-        <OrderTable orderList={splitPage} />
-        <Pagenation orderList={data} />
-      </section>
+      <ToolSection>
+        <SearchTool />
+      </ToolSection>
+      <TableSection>
+        <OrderTable orderList={fabricatedData?.slice(startPageNumber, endPageNumber)} />
+        <Pagenation orderList={fabricatedData} />
+      </TableSection>
     </MainLayout>
   );
 };
 
 export default HomePage;
+
+const ToolSection = styled.section`
+  width: 100%;
+`;
+
+const TableSection = styled.section`
+  width: 100%;
+  height: 80%;
+`;
