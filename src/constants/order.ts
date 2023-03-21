53006933365a5ce554fa94137b35a97a1da95703
hongsoom <hongsoom@kakao.com>
2023-03-21 15:18:21 +0900

    [#2] feature : 많이 사용하는 상수 값 파일 생성
    매번 문자열로 쓰기 보다는 미리 지정해둔 상수값으로 활용하기 위해 상수값
    파일 생성

---
+++ src/constants/order.ts
@@ -0,0 +1,17 @@
+export const QUERY_KEY = {
+  page: 'page',
+  sort: 'sort',
+  id: 'id',
+  time: 'time',
+};
+
+export const SORT_TYPE = {
+  id_desc: 'id:desc',
+  id_asc: 'id:asc',
+  time_desc: 'time:desc',
+  time_asc: 'time:asc',
+};
+
+export const TODAY = '2023-03-08';
+
+export const SINGLE_PAGE_SIZE = 50;
