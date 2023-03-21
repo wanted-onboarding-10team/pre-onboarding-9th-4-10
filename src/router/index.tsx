24e3568c610b399e4d6584ac60cb36a216029e85
hongsoom <hongsoom@kakao.com>
2023-03-21 15:04:05 +0900

    [#2] feature : 라우터 설정

---
+++ src/router/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { createBrowserRouter } from 'react-router-dom';
+import MainPage from 'pages/MainPage';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <MainPage />,
+  },
+]);
+
+export default router;
