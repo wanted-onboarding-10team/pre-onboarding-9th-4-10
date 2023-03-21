3939ff6403e1435dcf1f1adf40f3a691119553d5
suwan <skdoqj@naver.com>
2023-03-22 00:05:48 +0900

    [#2 #3] feature : 오늘의 거래건, 이름 검색 기능 추가 날짜 초기값을 3월 8일로 설정했습니다
    캘린더 라이브러리를 사용해서 다른날짜 선택도 가능하도록 구현했습니다
    
    이름 검색은 ant design의 table 컴포넌트를 이용해서 구현했습니다

--- src/pages/OderListPage.tsx
+++ src/pages/OderListPage.tsx
@@ -1,16 +1,124 @@
+import { useState, useRef } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import { Data, TableColums } from 'types/types';
 
 import type { DatePickerProps } from 'antd';
-import { DatePicker, Space } from 'antd';
+import { DatePicker } from 'antd';
 
-import { Table } from 'antd';
-import type { ColumnsType, TableProps } from 'antd/es/table';
+import { Button, Input, Space, Table, InputRef } from 'antd';
+import type { ColumnsType, ColumnType, TableProps } from 'antd/es/table';
+import type { FilterConfirmProps } from 'antd/es/table/interface';
 import { text } from 'stream/consumers';
+import { SearchOutlined } from '@ant-design/icons';
+import Highlighter from 'react-highlight-words';
+import dayjs from 'dayjs';
 
 const OrderListPage = () => {
   const data = useLoaderData() as Data[];
   //   console.log(data);
+
+  const [searchText, setSearchText] = useState('');
+  const [searchedColumn, setSearchedColumn] = useState('');
+  const searchInput = useRef<InputRef>(null);
+
+  const [selectDate, setSelectDate] = useState('2023-03-08');
+
+  const selecetedData = data.filter(item => item.transaction_date === selectDate);
+  console.log(selecetedData);
+
+  type DataIndex = keyof Data;
+
+  const handleSearch = (
+    selectedKeys: string[],
+    confirm: (param?: FilterConfirmProps) => void,
+    dataIndex: DataIndex,
+  ) => {
+    confirm();
+    setSearchText(selectedKeys[0]);
+    setSearchedColumn(dataIndex);
+  };
+
+  const handleReset = (clearFilters: () => void) => {
+    clearFilters();
+    setSearchText('');
+  };
+
+  const getColumnSearchProps = (dataIndex: DataIndex): ColumnType<Data> => ({
+    filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters, close }) => (
+      <div style={{ padding: 8 }} onKeyDown={e => e.stopPropagation()}>
+        <Input
+          ref={searchInput}
+          placeholder={`Search ${dataIndex}`}
+          value={selectedKeys[0]}
+          onChange={e => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+          onPressEnter={() => handleSearch(selectedKeys as string[], confirm, dataIndex)}
+          style={{ marginBottom: 8, display: 'block' }}
+        />
+        <Space>
+          <Button
+            type='primary'
+            onClick={() => handleSearch(selectedKeys as string[], confirm, dataIndex)}
+            icon={<SearchOutlined />}
+            size='small'
+            style={{ width: 90 }}
+          >
+            Search
+          </Button>
+          <Button
+            onClick={() => clearFilters && handleReset(clearFilters)}
+            size='small'
+            style={{ width: 90 }}
+          >
+            Reset
+          </Button>
+          <Button
+            type='link'
+            size='small'
+            onClick={() => {
+              confirm({ closeDropdown: false });
+              setSearchText((selectedKeys as string[])[0]);
+              setSearchedColumn(dataIndex);
+            }}
+          >
+            Filter
+          </Button>
+          <Button
+            type='link'
+            size='small'
+            onClick={() => {
+              close();
+            }}
+          >
+            close
+          </Button>
+        </Space>
+      </div>
+    ),
+    filterIcon: (filtered: boolean) => (
+      <SearchOutlined style={{ color: filtered ? '#1890ff' : undefined }} />
+    ),
+    onFilter: (value: any, record: any) =>
+      record[dataIndex]
+        .toString()
+        .toLowerCase()
+        .includes((value as string).toLowerCase()),
+    onFilterDropdownOpenChange: visible => {
+      if (visible) {
+        setTimeout(() => searchInput.current?.select(), 100);
+      }
+    },
+    render: text =>
+      searchedColumn === dataIndex ? (
+        <Highlighter
+          highlightStyle={{ backgroundColor: '#ffc069', padding: 0 }}
+          searchWords={[searchText]}
+          autoEscape
+          textToHighlight={text ? text.toString() : ''}
+        />
+      ) : (
+        text
+      ),
+  });
 
   const columns: ColumnsType<Data> = [
     {
@@ -39,31 +147,30 @@ const OrderListPage = () => {
       onFilter: (value, record): boolean => record.status === value,
     },
     { title: '고객번호', dataIndex: 'customer_id', key: 'customer_id' },
-    { title: '고객이름', dataIndex: 'customer_name', key: 'customer_name' },
+    {
+      title: '고객이름',
+      dataIndex: 'customer_name',
+      key: 'customer_name',
+      ...getColumnSearchProps('customer_name'),
+    },
     { title: '가격', dataIndex: 'currency', key: 'currency' },
   ];
 
+  // 날짜 선택
   const dateOnChange: DatePickerProps['onChange'] = (date, dateString) => {
-    console.log(date, dateString);
+    console.log(dateString);
+    setSelectDate(dateString);
   };
 
-  //   const tableOnChange: TableProps<DataType>['onChange'] = (pagination, filters, sorter, extra) => {
-  //     console.log('params', pagination, filters, sorter, extra);
-  //   };
-
+  const defaultDate = dayjs('2023-03-08');
   return (
     <>
-      <h1>hello</h1>
+      <span>선택한 날짜</span>
       <Space direction='vertical'>
-        <DatePicker onChange={dateOnChange} />
+        <DatePicker onChange={dateOnChange} defaultValue={defaultDate} />
       </Space>
 
-      <Table
-        columns={columns}
-        dataSource={data}
-        //   onChange={tableOnChange}
-        pagination={{ pageSize: 50 }}
-      />
+      <Table columns={columns} dataSource={selecetedData} pagination={{ pageSize: 50 }} />
     </>
   );
 };
