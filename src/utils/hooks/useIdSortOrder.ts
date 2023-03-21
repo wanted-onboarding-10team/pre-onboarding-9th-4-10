9cfcdb8483eac8c477f2e26ea5ce630b41d80db5
yoosion030 <rsy5487@naver.com>
2023-03-21 13:29:49 +0900

    [#3] fix: sort시 바로 state에 반영이 안되던 오류 해결

--- src/utils/hooks/useIdSortOrder.ts
+++ src/utils/hooks/useIdSortOrder.ts
@@ -3,9 +3,9 @@ import { SortType } from 'types/sort';
 
 const useIdSortOrder = (data: OrderDataType[], sort: SortType) => {
   if (sort === 'asc') {
-    return data.sort((a, b) => a.id - b.id);
+    return [...data].sort((a, b) => a.id - b.id);
   }
-  return data.sort((a, b) => b.id - a.id);
+  return [...data].sort((a, b) => b.id - a.id);
 };
 
 export default useIdSortOrder;
--- src/utils/hooks/useTimeSortOrder.ts
+++ src/utils/hooks/useTimeSortOrder.ts
@@ -2,7 +2,7 @@ import { OrderDataType } from 'types/order';
 import { SortType } from 'types/sort';
 
 const useTimeSortOrder = (data: OrderDataType[], sort: SortType) => {
-  return data.sort((a, b) => {
+  return [...data].sort((a, b) => {
     if (sort === 'asc') {
       return new Date(a.transaction_time).getTime() - new Date(b.transaction_time).getTime();
     } else {
