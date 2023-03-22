4b110b91fc88887eeb8a63c5a8a57b4b8fafab8a
suwan <skdoqj@naver.com>
2023-03-22 17:56:39 +0900

    [#3] feature : api 5초마다 업데이트 setInverval 을 사용했습니다

--- src/router/loader/mainLoader.ts
+++ src/router/loader/mainLoader.ts
@@ -1,19 +1,23 @@
 import { getDataApi } from 'util/api/data';
 
 const mainLoader = async () => {
-  const { data: response } = await getDataApi();
+  try {
+    const { data: response } = await getDataApi();
 
-  const data = response.map((item: any) => {
-    return {
-      ...item,
-      transaction_date: item.transaction_time.split(' ')[0],
-      transaction_time: item.transaction_time.split(' ')[1],
-      time: item.transaction_time.split(' ')[1].replace(/:/gi, ''),
-    };
-  });
-  console.log(data);
+    const data = response.map((item: any) => {
+      return {
+        ...item,
+        transaction_date: item.transaction_time.split(' ')[0],
+        transaction_time: item.transaction_time.split(' ')[1],
+        time: item.transaction_time.split(' ')[1].replace(/:/gi, ''),
+      };
+    });
 
-  return data;
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
 };
+setInterval(mainLoader, 5000);
 
 export default mainLoader;
--- src/util/api/data.ts
+++ src/util/api/data.ts
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { Data } from 'types/types';
 
 export const getDataApi = async () => {
   return await axios.get(`/data/mock_data.json`);
