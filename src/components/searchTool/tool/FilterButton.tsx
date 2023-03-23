450c6e39ed20b566a8ded499f602db419d38df2a
yoosion030 <rsy5487@naver.com>
2023-03-23 14:30:38 +0900

    [#18] fix: 필터링 버튼 클릭 시 해당 필터링 상태이면 하이라이팅 처리
    isDisabled를 사용하여 query 값을 가져와 하이라이팅 판별을 하였습니다.

--- src/components/searchTool/tool/FilterButton.tsx
+++ src/components/searchTool/tool/FilterButton.tsx
@@ -3,7 +3,7 @@ import { useSearchParams } from 'react-router-dom';
 
 const FilterButton = () => {
   const [query, setQuery] = useSearchParams();
-
+  const status = query.get('status');
   const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     const prevQuery = Array.from(query).reduce((prev: { [index: string]: string }, current) => {
       prev[current[0]] = current[1];
@@ -17,10 +17,10 @@ const FilterButton = () => {
 
   return (
     <ButtonGroup spacing='30px'>
-      <Button value='true' onClick={handleClick}>
+      <Button value='true' onClick={handleClick} isDisabled={status === 'true'}>
         완료목록
       </Button>
-      <Button value='false' onClick={handleClick}>
+      <Button value='false' onClick={handleClick} isDisabled={status === 'false'}>
         미완료목록
       </Button>
       <Button onClick={handleClick}>전체</Button>
